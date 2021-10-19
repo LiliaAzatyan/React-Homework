46a3f7a5a7a3087842e3c1d355fae581fbb2c6d4
LiliaAzatyan <azatyanlilia@gmail.com>
2021-10-19 17:11:55 +0400

    first test

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+         rules: [
+             {
+                test: /.(jsx?)$/,
+                use: 'babel-loader',
+             },
+             {
+                 test: /.scss$/,
+                 use: [
+                     {
+                        loader: MiniCssExtractPlugin.loader,
+                     },
+                     {
+                         loader: 'css-loader',
+                         options: {
+                             importLoaders: 1,
+                             modules: {
+                                 localIdentName: '[path][name]__[local]',
+                             },
+                         },
+                     },
+                     'sass-loader',
+                 ]
+             }
+         ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name.css]',
+        }),
+       // new webpack.DefinePlugin({
+         //   'process.env.NODE_ENV': JSON.Stringify('production'),
+        //})
+    ],
+}
